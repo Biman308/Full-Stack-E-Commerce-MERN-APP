2561fc885bf00f9325bb0df760c4f9cd6f2f8c40
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 20:20:08 +0530

    Update permission.js

--- backend/helpers/permission.js
+++ backend/helpers/permission.js
@@ -1,4 +1,4 @@
-const userModel = require("../models/userModel")
+const userModel = require("../models/userModels")
 
 const uploadProductPermission = async(userId) => {
     const user = await userModel.findById(userId)
