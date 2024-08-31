c7a9138715a26edeb8166a3b2436f62e2b30dbff
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 20:12:52 +0530

    Update getCategoryProductOne.js

--- backend/controller/product/getCategoryProductOne.js
+++ backend/controller/product/getCategoryProductOne.js
@@ -1,37 +1,39 @@
 const productModel = require("../../models/productModel")
 
 
-const getCategoryProduct = async(req,res)=>{
-    try{
+const getCategoryProduct = async (req, res) => {
+    try {
         const productCategory = await productModel.distinct("category")
+        // Retrieves a list of distinct (unique) categories from the productModel collection. 
 
-        console.log("category",productCategory)
+        console.log("category", productCategory)
 
-        //array to store one product from each category
         const productByCategory = []
 
-        for(const category of productCategory){
-            const product = await productModel.findOne({category })
+        for (const category of productCategory) {
+            // Iterates over each category in the productCategory array.
+            const product = await productModel.findOne({ category })
+            // Searches for one product that matches the current category.
 
-            if(product){
+            if (product) {
                 productByCategory.push(product)
+                // Adds the found product to the productByCategory array if a product exists.
             }
         }
 
-
         res.json({
-            message : "category product",
-            data : productByCategory,
-            success : true,
-            error : false
+            message: "category product",
+            data: productByCategory,
+            success: true,
+            error: false
         })
 
 
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
-            message : err.message || err,
-            error : true,
-            success : false
+            message: err.message || err,
+            error: true,
+            success: false
         })
     }
 }
