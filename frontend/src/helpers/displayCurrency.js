8983b7e7a0206203a883a557d3d0141d600ec445
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 21:11:50 +0530

    Update displayCurrency.js

--- frontend/src/helpers/displayCurrency.js
+++ frontend/src/helpers/displayCurrency.js
@@ -1,8 +1,8 @@
 const displayINRCurrency = (num) => {
-    const formatter = new Intl.NumberFormat('en-IN',{
-        style : "currency",
-        currency : 'INR',
-        minimumFractionDigits : 2
+    const formatter = new Intl.NumberFormat('en-IN', {
+        style: "currency", // Specifies that the number should be formatted as currency
+        currency: 'INR',
+        minimumFractionDigits: 2 // Ensures that there are always two decimal places
     })
 
     return formatter.format(num)
