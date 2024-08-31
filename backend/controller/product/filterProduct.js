c7bda3a078bd8020300daa86f9eb54f635df2349
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 20:12:26 +0530

    Update filterProduct.js

--- backend/controller/product/filterProduct.js
+++ backend/controller/product/filterProduct.js
@@ -1,29 +1,35 @@
 const productModel = require("../../models/productModel")
 
-const filterProductController = async(req,res)=>{
- try{
-        const categoryList = req?.body?.category || []
+const filterProductController = async (req, res) => {
+    try {
+        const categoryList = req?.body?.category || []  //accessing req.body.category.  
+        // If req.body.category is undefined or null, it defaults to an empty array ([]).
 
         const product = await productModel.find({
-            category :  {
-                "$in" : categoryList
+            // await: Pauses the function until the database query completes.
+            category: {
+                "$in": categoryList
             }
+            // Calls the find method on the productModel to search for 
+            // the documents in the MongoDB collection.
         })
 
         res.json({
-            data : product,
-            message : "product",
-            error : false,
-            success : true
+            // res.json: Sends a JSON response to the client.
+            data: product,
+            message: "product",
+            error: false,
+            success: true
         })
- }catch(err){
-    res.json({
-        message : err.message || err,
-        error : true,
-        success : false
-    })
- }
-}
 
+    } catch (err) {
+        // catch (err): Catches any errors that occurred in the try block.
+        res.json({
+            message: err.message || err,
+            error: true,
+            success: false
+        })
+    }
+}
 
 module.exports = filterProductController
