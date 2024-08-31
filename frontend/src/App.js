04a282e5f6786ee7a7fd5371ade6c680058ec84c
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 21:17:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
@@ -13,25 +12,25 @@ import { setUserDetails } from './store/userSlice';
 
 function App() {
   const dispatch = useDispatch()
-  const [cartProductCount,setCartProductCount] = useState(0)
+  const [cartProductCount, setCartProductCount] = useState(0)
 
-  const fetchUserDetails = async()=>{
-      const dataResponse = await fetch(SummaryApi.current_user.url,{
-        method : SummaryApi.current_user.method,
-        credentials : 'include'
-      })
+  const fetchUserDetails = async () => {
+    const dataResponse = await fetch(SummaryApi.current_user.url, {
+      method: SummaryApi.current_user.method,
+      credentials: 'include'
+    })
 
-      const dataApi = await dataResponse.json()
+    const dataApi = await dataResponse.json()
 
-      if(dataApi.success){
-        dispatch(setUserDetails(dataApi.data))
-      }
+    if (dataApi.success) {
+      dispatch(setUserDetails(dataApi.data))
+    }
   }
 
-  const fetchUserAddToCart = async()=>{
-    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
-      method : SummaryApi.addToCartProductCount.method,
-      credentials : 'include'
+  const fetchUserAddToCart = async () => {
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url, {
+      method: SummaryApi.addToCartProductCount.method,
+      credentials: 'include'
     })
 
     const dataApi = await dataResponse.json()
@@ -39,29 +38,29 @@ function App() {
     setCartProductCount(dataApi?.data?.count)
   }
 
-  useEffect(()=>{
-    /**user Details */
-    fetchUserDetails()
-    /**user Details cart product */
+  useEffect(() => {
+    fetchUserDetails();
     fetchUserAddToCart()
 
-  },[])
+  }, []);
+
   return (
     <>
       <Context.Provider value={{
-          fetchUserDetails, // user detail fetch 
-          cartProductCount, // current user add to cart product count,
-          fetchUserAddToCart
+        // Context is the context object created using React.createContext().
+        fetchUserDetails,
+        cartProductCount,
+        fetchUserAddToCart
       }}>
-        <ToastContainer 
+        <ToastContainer
           position='top-center'
         />
-        
-        <Header/>
-        <main className='min-h-[calc(100vh-120px)] pt-16'>
-          <Outlet/>
+
+        <Header />
+        <main className='min-h-[calc(100vh-120px)] pt-32'>
+          <Outlet />
         </main>
-        <Footer/>
+        <Footer />
       </Context.Provider>
     </>
   );
