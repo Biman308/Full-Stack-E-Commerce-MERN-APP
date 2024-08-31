7d3445a402f43e9da21f2e08bf5cfc5a6f746e8c
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 20:18:46 +0530

    Update userLogout.js

--- backend/controller/user/userLogout.js
+++ backend/controller/user/userLogout.js
@@ -1,21 +1,23 @@
-async function userLogout(req,res){
-    try{
+async function userLogout(req, res) {
+    try {
         res.clearCookie("token")
+        // This is a method provided by Express.js to remove a cookie that was previously set in the client's browser.
+        // "token": This is the name of the cookie that you want to clear.
 
         res.json({
-            message : "Logged out successfully",
-            error : false,
-            success : true,
-            data : []
+            message: "Logged out successfully",
+            error: false,
+            success: true,
+            data: []
         })
-    }catch(err){
+    } catch (err) {
         res.json({
-            message : err.message || err  ,
-            error : true,
-            success : false,
+            message: err.message || err,
+            error: true,
+            success: false,
         })
     }
 }
 
 
-module.exports = userLogout
+module.exports = userLogout;
