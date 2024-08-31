807f17c1699a1fe89e4fe1717c8458766fb59ff8
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 20:17:55 +0530

    Update updateUser.js

--- backend/controller/user/updateUser.js
+++ backend/controller/user/updateUser.js
@@ -1,40 +1,39 @@
-const userModel = require("../../models/userModel")
+const userModel = require("../../models/userModels")
 
-async function updateUser(req,res){
-    try{
+async function updateUser(req, res) {
+    try {
         const sessionUser = req.userId
+        // xtracts the userId from the request object
 
-        const { userId , email, name, role} = req.body
+        const { userId, email, name, role } = req.body
+        // userId, email, name, and role properties from the req.body object.
 
         const payload = {
-            ...( email && { email : email}),
-            ...( name && { name : name}),
-            ...( role && { role : role}),
+            ...(email && { email: email }),
+            // If email is truthy then email field is added to the payload object. as for all below.
+            ...(name && { name: name }),
+            ...(role && { role: role }),
         }
 
         const user = await userModel.findById(sessionUser)
 
-        console.log("user.role",user.role)
+        console.log("user.role", user.role)
 
+        const updateUser = await userModel.findByIdAndUpdate(userId, payload)
 
-
-        const updateUser = await userModel.findByIdAndUpdate(userId,payload)
-
-        
         res.json({
-            data : updateUser,
-            message : "User Updated",
-            success : true,
-            error : false
+            data: updateUser,
+            message: "User Updated",
+            success: true,
+            error: false
         })
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
-            message : err.message || err,
-            error : true,
-            success : false
+            message: err.message || err,
+            error: true,
+            success: false
         })
     }
 }
-
 
 module.exports = updateUser
