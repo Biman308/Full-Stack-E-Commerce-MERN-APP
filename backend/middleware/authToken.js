d8c43b602b2deab4dd9e1c761d1e48d02bd7cfcb
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 20:20:29 +0530

    Update authToken.js

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -1,23 +1,25 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-async function authToken(req,res,next){
-    try{
+async function authToken(req, res, next) {
+    try {
         const token = req.cookies?.token
+        // extract a cookie named token from the request object in an Express.js application.
 
-        console.log("token",token)
-        if(!token){
+        console.log("token", token)
+        if (!token) {
+            // if token is undefined, null, an empty string, 0, NaN, or false
             return res.status(200).json({
-                message : "Please Login...!",
-                error : true,
-                success : false
+                message: "Please Login!",
+                error: true,
+                success: false
             })
         }
 
-        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
+        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function (err, decoded) {
             console.log(err)
-            console.log("decoded",decoded)
-            
-            if(err){
+            console.log("decoded", decoded)
+
+            if (err) {
                 console.log("error auth", err)
             }
 
@@ -27,12 +29,12 @@ async function authToken(req,res,next){
         });
 
 
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
-            message : err.message || err,
-            data : [],
-            error : true,
-            success : false
+            message: err.message || err,
+            data: [],
+            error: true,
+            success: false
         })
     }
 }
