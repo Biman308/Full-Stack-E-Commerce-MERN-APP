26d1530a64511a5d8a91759c22f946f80b5731f3
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-08-31 20:13:45 +0530

    Update getProduct.js

--- backend/controller/product/getProduct.js
+++ backend/controller/product/getProduct.js
@@ -1,21 +1,24 @@
 const productModel = require("../../models/productModel")
 
-const getProductController = async(req,res)=>{
-    try{
-        const allProduct = await productModel.find().sort({ createdAt : -1 })
+const getProductController = async (req, res) => {
+    try {
+        const allProduct = await productModel.find().sort({ createdAt: -1 })
+        // This means the most recently created products will appear first.
 
         res.json({
-            message : "All Product",
-            success : true,
-            error : false,
-            data : allProduct
+            message: "All Product",
+            success: true,
+            error: false,
+            data: allProduct
         })
 
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
-            message : err.message || err,
-            error : true,
-            success : false
+            // Sends a JSON response with an HTTP status code of 400 (Bad Request), 
+            // indicating that something went wrong.
+            message: err.message || err,   // message is not available.
+            error: true, 
+            success: false
         })
     }
 
