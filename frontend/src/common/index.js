03429187c61bf769ea7fbeffebecfa49d99057bf
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-09-01 21:18:20 +0530

    Update index.js

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -2,79 +2,79 @@
 
 const SummaryApi = {
     signUP: {
-        url: 'http://localhost:8080/api/signup',
+        url: 'https://shopping-app-three-blue.vercel.app/api/signup',
         method: 'POST',
     },
     signIn: {
-        url: 'http://localhost:8080/api/signin',
+        url: 'https://shopping-app-three-blue.vercel.app/api/signin',
         method: 'POST',
     },
     current_user: {
-        url: 'http://localhost:8080/api/user-details',
+        url: 'https://shopping-app-three-blue.vercel.app/api/user-details',
         method: "GET"
     },
     logout_user: {
-        url: 'http://localhost:8080/api/userLogout',
+        url: 'https://shopping-app-three-blue.vercel.app/api/userLogout',
         method: "GET"
     },
     allUser: {
-        url: 'http://localhost:8080/api/all-user',
+        url: 'https://shopping-app-three-blue.vercel.app/api/all-user',
         method: 'GET'
     },
     updateUser: {
-        url: 'http://localhost:8080/api/update-user',
+        url: 'https://shopping-app-three-blue.vercel.app/api/update-user',
         method: "POST"
     },
     uploadProduct: {
-        url: 'http://localhost:8080/api/upload-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/upload-product',
         method: 'POST'
     },
     allProduct: {
-        url: 'http://localhost:8080/api/get-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/get-product',
         method: 'GET'
     },
     updateProduct: {
-        url: 'http://localhost:8080/api/update-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/update-product',
         method: 'POST'
     },
     categoryProduct: {
-        url: 'http://localhost:8080/api/get-categoryProduct',
+        url: 'https://shopping-app-three-blue.vercel.app/api/get-categoryProduct',
         method: 'GET'
     },
     categoryWiseProduct: {
-        url: 'http://localhost:8080/api/category-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/category-product',
         method: 'POST'
     },
     productDetails: {
-        url: 'http://localhost:8080/api/product-details',
+        url: 'https://shopping-app-three-blue.vercel.app/api/product-details',
         method: 'POST'
     },
     addToCartProduct: {
-        url: 'http://localhost:8080/api/addtocart',
+        url: 'https://shopping-app-three-blue.vercel.app/api/addtocart',
         method: 'POST'
     },
     addToCartProductCount: {
-        url: 'http://localhost:8080/api/countAddToCartProduct',
+        url: 'https://shopping-app-three-blue.vercel.app/api/countAddToCartProduct',
         method: 'GET'
     },
     addToCartProductView: {
-        url: 'http://localhost:8080/api/view-cart-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/view-cart-product',
         method: 'GET'
     },
     updateCartProduct: {
-        url: 'http://localhost:8080/api/update-cart-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/update-cart-product',
         method: 'POST'
     },
     deleteCartProduct: {
-        url: 'http://localhost:8080/api/delete-cart-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/delete-cart-product',
         method: 'POST'
     },
     searchProduct: {
-        url: 'http://localhost:8080/api/search',
+        url: 'https://shopping-app-three-blue.vercel.app/api/search',
         method: 'GET'
     },
     filterProduct: {
-        url: 'http://localhost:8080/api/filter-product',
+        url: 'https://shopping-app-three-blue.vercel.app/api/filter-product',
         method: 'POST'
     }
 }
