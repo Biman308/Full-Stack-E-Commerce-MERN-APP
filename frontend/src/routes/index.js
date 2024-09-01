a7cc4fad8bcfb071cdccefee80a63edb3fcdd677
Biman Patra <119288685+Biman308@users.noreply.github.com>
2024-09-01 08:57:17 +0530

    Update index.js

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
-import ForgotPassowrd from '../pages/ForgotPassowrd';
+import ForgotPasswrd from '../pages/ForgotPasswrd';
 import SignUp from '../pages/SignUp';
 import AdminPanel from '../pages/AdminPanel';
 import AllUsers from '../pages/AllUsers';
@@ -27,7 +27,7 @@ const Router = createBrowserRouter([  // Defining the Router
             },
             {
                 path: "forgot-password",
-                element: <ForgotPassowrd />
+                element: <ForgotPasswrd />
             },
             {
                 path: "sign-up",
